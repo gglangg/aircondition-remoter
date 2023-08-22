662e5a1c2e6c0e0ff88b97cdc5337b0bb6ded28d
Gene <clhuang@pllab.cs.nthu.edu.tw>
2023-08-22 13:10:34 +0800

    first commit

---
+++ server.py
@@ -0,0 +1,173 @@
+from flask import Flask,request,render_template,jsonify
+
+import datetime
+
+
+
+# 取得現在的日期和時間
+
+import os
+
+
+
+def returnUpdateTime():
+
+    now = datetime.datetime.now()
+
+    return 'latest update {}'.format(now.strftime("%m-%d %H:%M:%S")) 
+
+
+
+
+
+PREFIX_CMD = "/usr/bin/irsend SEND_ONCE AIR_CONDITIONER "
+
+
+
+TEMPERATURE = 25
+
+MODE = '冷氣'
+
+LATEST_UPDATE = returnUpdateTime()
+
+
+
+app = Flask(__name__)
+
+
+
+KEY = 'oqaijOQI'
+
+
+
+
+
+@app.route('/')
+
+def home():
+
+
+
+    key = request.args.get('key', 0)
+
+
+
+    if key==KEY:
+
+
+
+        return render_template('template.html', key=KEY, msg=LATEST_UPDATE,mode=MODE,temperature=TEMPERATURE)
+
+
+
+    return '<h1>Invalid Key</h1>'
+
+
+
+
+
+@app.route('/run')
+
+def about():
+
+    global TEMPERATURE, MODE, LATEST_UPDATE
+
+
+
+    key = request.args.get('key', 0)
+
+
+
+    cmd = request.args.get('cmd', 0)
+
+    if 'HEAT' in cmd:
+
+        return render_template('template.html', key=KEY, msg=LATEST_UPDATE+' HEAT disable',mode=MODE,temperature=TEMPERATURE)
+
+    if cmd=='POWER_OFF':
+        LATEST_UPDATE = 'POWER_OFF'
+        os.system( PREFIX_CMD + cmd)
+        return render_template('template.html', key=KEY, msg=LATEST_UPDATE,mode=MODE,temperature=TEMPERATURE)
+        
+
+    TEMPERATURE = cmd.split('t')[-1]
+
+    M = cmd.split('_')[0]
+
+    if M=='COOL':
+
+        MODE = '冷氣'
+
+    elif M=='DRY':
+
+        MODE = '除濕'
+
+    elif M=='HEAT':
+
+        MODE = '暖氣'
+
+        
+
+    if key==KEY:
+
+        os.system( PREFIX_CMD + cmd)
+
+        LATEST_UPDATE = returnUpdateTime()
+
+        return render_template('template.html', key=KEY, msg=LATEST_UPDATE,mode=MODE,temperature=TEMPERATURE)
+
+
+
+    return '<h1>Invalid Key</h1>'
+
+
+
+
+
+@app.route('/update')
+
+def update():
+
+
+
+    key = request.args.get('key', 0)
+
+    if key==KEY:
+
+
+
+        # LATEST_UPDATE = returnUpdateTime()
+
+        data = {
+
+            'msg': LATEST_UPDATE,
+
+            'mode': MODE,
+
+            'temperature': TEMPERATURE,
+
+        }    
+
+        return jsonify(data)
+
+
+
+    
+
+    return '<h1>Invalid Key</h1>'
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+
+
+    app.run(host='0.0.0.0', port=8080)
+
+
